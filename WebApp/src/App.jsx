7e60476440c6ee579f5a4544e48426405d0ac3b3
kappejoh <kaspar.wale@gmail.com>
2025-02-05 10:19:56 +0100

    added routing

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Projects from './Views/Projects'
+import CreateProject from './Views/CreateProject'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/projects" element={<Projects />} ></Route>
+        <Route path="/projects/create" element={<CreateProject />} ></Route>
+        <Route path="/" element={<CreateProject />} ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
